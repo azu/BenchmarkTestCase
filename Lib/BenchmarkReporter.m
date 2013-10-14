38040c886123434e0b4547b9d9bcff6740f3447f
azu <azuciao@gmail.com>
2013-10-14 17:51:31 +0900

    update reporter

--- Lib/BenchmarkReporter.m
+++ Lib/BenchmarkReporter.m
@@ -17,14 +17,15 @@
     NSString *header = [NSString stringWithFormat:@"\n%-50s %-10s %-10s",
                                                   [@":Name" UTF8String], [@":Total(s)" UTF8String], [@":Avg(s)" UTF8String]];
     NSMutableArray *lineArray = [NSMutableArray arrayWithObject:header];
-    NSString *lineFormat = @"%-50s %-10.5f %-10.5f";
+    NSString *lineFormat = @"%-50s %-10.5f %-10.5f %@";
     for (XCTestRun *xcTestRun in testRuns) {
         NSUInteger repeatCount = [[xcTestRun.test class] benchmarkRepeatCount];
         NSTimeInterval totalTimeInterval = [xcTestRun.stopDate timeIntervalSinceDate:xcTestRun.startDate];
         NSString *lineLog = [NSString stringWithFormat:lineFormat,
                                                        [xcTestRun.test.name UTF8String],
                                                        totalTimeInterval,
-                                                       totalTimeInterval / repeatCount];
+                                                       totalTimeInterval / repeatCount,
+                                                       [NSString stringWithFormat:@"(1/%d)", repeatCount]];
 
         [lineArray addObject:lineLog];
     }
